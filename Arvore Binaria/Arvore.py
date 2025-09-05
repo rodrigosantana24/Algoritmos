a09b9335c1d2e573044615739456500f71732f66
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-09-05 15:32:29 -0300

    Método inserir

--- Arvore Binaria/Arvore.py
+++ Arvore Binaria/Arvore.py
@@ -9,3 +9,23 @@ class Tree:
         self.raiz = No(None, None, None)
         self.raiz = None
     
+    def inserir(self, valor):
+        novo = No(valor, None, None)
+        if self.raiz == None:
+            self.raiz = novo
+        else:
+            atual = self.raiz
+            while True:
+                anterior = atual
+                if valor <= atual.item:
+                    atual = atual.esquerda
+                    if atual == None:
+                        anterior.esquerda = novo
+                        return
+                else:
+                    atual = atual.direita
+                    if atual == None:
+                        anterior.direita = novo
+                        return
+    
+
