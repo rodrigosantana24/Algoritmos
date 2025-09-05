f07e04b3748d4bd9816b57af13283263b28b3002
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-09-05 17:00:57 -0300

    Método precessor

--- Arvore Binaria/Arvore.py
+++ Arvore Binaria/Arvore.py
@@ -6,7 +6,6 @@ class No:
     
 class Tree:
     def __init__(self):
-        self.raiz = No(None, None, None)
         self.raiz = None
     
     def inserir(self, valor):
@@ -70,4 +69,53 @@ class Tree:
                 atual = atual.direita
             if atual == None:
                 return False
+        if atual.esquerda == None and atual.direita == None:
+            if atual == self.raiz:
+                self.raiz = None
+            else:
+                if filho_esquerdo:
+                    pai.esquerda = None
+                else:
+                    pai.direita = None
+        elif atual.direita == None:
+            if atual == self.raiz:
+                self.raiz = atual.esquerda
+            else:
+                if filho_esquerdo:
+                    pai.esquerda = atual.esquerda
+                else:
+                    pai.direita = atual.esquerda
+        elif atual.esquerda == None:
+            if atual == self.raiz:
+                self.raiz = atual.direita
+            else:
+                if filho_esquerdo:
+                        pai.esquerda = atual.direita 
+                else:
+                        pai.direita = atual.direita 
+        else:
+            sucessor = self.sucessor(atual)
+            if atual == self.raiz:
+                    self.raiz = sucessor 
+            else:
+                    if filho_esquerdo:
+                        pai.esquerda = sucessor 
+                    else:
+                        pai.direita = sucessor 
+            sucessor.esquerda = atual.esquerda 
+        return True
+    
+    def precessor(self, no):
+        pai_do_precessor = no
+        precessor = no
+        atual = no.esquerda
+        while atual is not None:
+            pai_do_precessor = precessor
+            precessor = atual
+            atual = atual.direita
+        if precessor != no.esquerda:
+            pai_do_precessor.direita = precessor.esquerda
+            precessor.esquerda = no.esquerda
+        return precessor
+
 
