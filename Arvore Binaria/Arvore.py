cf0975a1d3bb5611b4648b1827f64a0cfc57234d
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-09-05 15:31:54 -0300

    Árvore binária em Python

---
+++ Arvore Binaria/Arvore.py
@@ -0,0 +1,11 @@
+class No:
+    def __init__(self, chave, esquerda, direita):
+        self.item = chave
+        self.esquerda = esquerda
+        self.direita = direita
+    
+class Tree:
+    def __init__(self):
+        self.raiz = No(None, None, None)
+        self.raiz = None
+    
